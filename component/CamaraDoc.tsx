acce913842bdd4b3ba534ef260cd7df97be2badf
Ruddy paz <ruddypazd@gmail.com>
2024-01-15 10:20:09 -0400

    cambios en camara, falta para otros telefonos

--- component/CamaraDoc.tsx
+++ component/CamaraDoc.tsx
@@ -1,55 +1,38 @@
 
 
 import React, { useEffect, useRef, useState } from 'react';
-import { Button, Image, Text, View, PermissionsAndroid, Platform, Dimensions, TouchableOpacity } from 'react-native';
+import { Button, Image, Text, View, PermissionsAndroid, Platform, Dimensions, TouchableOpacity, StyleSheet } from 'react-native';
 import tema from '../enviroments/tema.json'
 import { Camera, useCameraDevice, useCameraFormat } from 'react-native-vision-camera';
 import ReadText from './ReadText';
 import ImagePicker from 'react-native-image-crop-picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Load from './Load';
 
 const CamaraDoc = (navigation: any) => {
   const device = useCameraDevice('back');
-  const format = useCameraFormat(device, [{ fps: 240 }]);
   const camera = useRef(null);
-  const [permiso, setPermiso] = useState(false);
-  const [state, setState] = useState(false);
+  const [state, setState] = useState({});
 
   const requestCameraPermissionAndroid = async () => {
     try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.CAMERA,
-        {
-          title: "Permiso de Cámara",
-          message: "Esta aplicación necesita acceso a tu cámara",
-          buttonNeutral: "Pregúntame Luego",
-          buttonNegative: "Cancelar",
-          buttonPositive: "Aceptar"
-        }
-      );
+      const granted = await Camera.requestCameraPermission();
+      console.log(granted);
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        setPermiso(true);
-        console.log("Tienes acceso a la cámara");
+        return true;
       } else {
-        console.log("Permiso de cámara denegado");
-        setPermiso(false);
+        return false;
       }
-      espera();
     } catch (err) {
       console.warn(err);
-      setPermiso(false);
-      setState(false);
+      return false;
     }
   };
 
-  const espera = async () => {
-    let documentos = await AsyncStorage.getItem("documentos");
-    try {
-      if (documentos) state["documentos"] = JSON.parse(documentos);
-    } catch (error) {}
-
-    setState(true);
-    console.log("llego a cambiar a true " + state);
+  const setShowCamera = async () => {
+    await setTimeout(()=>{
+      setState({...state, estado:true});
+    }, 1000);
   };
 
   const takePhoto = async () => {
@@ -57,7 +40,7 @@ const CamaraDoc = (navigation: any) => {
       try {
         const photo = await camera.current.takePhoto();
         croper(photo.path);
-        setState(false);
+        setState({...state, estado:false});
       } catch (error) {
         console.error(error);
         // Manejar el error de la captura de la foto
@@ -65,54 +48,85 @@ const CamaraDoc = (navigation: any) => {
     }
   };
 
-  const uploadPhoto = () => {
-    ImagePicker.openPicker({
-      freeStyleCropEnabled: true,
-      cropping: true
-    }).then(image => {
-      croper(image.path);
-      setState(false);
+  const croper = async (path:any) =>{
+    ImagePicker.openCropper({
+      path: "file://"+path,
+      width: 400,
+      height: 400,
+      freeStyleCropEnabled:true,
+      cropperToolbarTitle:"Recorte el documento",
+      cropping:true
+    }).then(async image => {
+
+      state["documentos"].map((doc:any)=>{
+        if(doc.ID == navigation.route.params.id){
+            doc["url"] = image.path;
+        }
+      })
+      await AsyncStorage.setItem("documentos", JSON.stringify(state["documentos"]));
+      navigation.navigation.replace("Emision")
+      //navigation.navigation.goBack();
+    }).catch(e=>{
+      setState({...state, estado:true});
     });
   };
 
-  const paint = () => {
-    if (permiso && state) {
-      return (
-        <View style={{ flex: 1 }}>
-          <Camera
-            ref={camera}
-            photo={true}
-            device={device}
-            isActive={state}
-            style={{ flex: 1, zIndex: 1 }}
-            exposure={2}
-            format={format}
-          />
-          <Button title="Tomar Foto" onPress={takePhoto} />
-          <Button title="Subir Foto" onPress={uploadPhoto} />
-        </View>
-      );
-    } else {
-      return <View style={{ backgroundColor: "red", flex: 1 }}></View>;
-    }
-  };
 
   useEffect(() => {
     navigation.navigation.setOptions({ headerShown: false });
-    if (Platform.OS === 'android') {
-      requestCameraPermissionAndroid();
-    }
+    const init= async()=>{
+
+      let documentos = await AsyncStorage.getItem("documentos");
+      try {
+        if (documentos) state["documentos"] = JSON.parse(documentos);
+      } catch (error) {}
+
+      let permiso = false;
+      if (Platform.OS === 'android') {
+        permiso = await requestCameraPermissionAndroid();
+      }
+      
+      setState({...state, permiso, estado:false})
+    };
+    init();
+    
   }, []);
 
-  if (navigation.route.params.url) {
-    return <Text style={{ color: tema.active }}>{navigation.route.params.url}</Text>;
-  }
+
 
   if (device == null) {
     return <View><Text style={{ color: tema.active }}>No tienes cámara</Text></View>;
   }
 
-  return paint();
+  if(!state.permiso) return <View><Text style={{ color: tema.active }}>No tienes permiso</Text></View>
+
+  if (navigation.route.params.url) {
+    return <Text style={{ color: tema.active }}>{navigation.route.params.url}</Text>;
+  }
+
+  return (
+    <View style={{flex:1, alignItems:'center' }}>
+      <Camera
+        ref={camera}
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={state.estado}
+        photo={true}
+        onError={(error) => {console.log(error)}}
+        onInitialized={() => {setShowCamera()}}
+        
+      />
+      
+      <TouchableOpacity 
+        onPress={takePhoto}
+        style={{ width:50, height:50, backgroundColor:"#ffffffaa", borderRadius:20, marginTop:Dimensions.get('window').height-100}}
+      >
+
+      </TouchableOpacity>
+      {/*<Button title="Subir Foto" onPress={uploadPhoto} />*/}
+    </View>
+  )
+
 };
 
 export default CamaraDoc;
